18a00048a97c19065a2d4894310f19de9571dee1
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-04-30 21:02:50 +0200

    factorl out themes

--- src/NerdyPriceChart3/NerdyPriceChart3.tsx
+++ src/NerdyPriceChart3/NerdyPriceChart3.tsx
@@ -10,8 +10,9 @@ import {zColor} from '@remotion/zod-types';
 
 // import {SimpleLineChart} from '../SimpleLineChart/SimpleLineChart';
 import {AnimatedLineChart2} from '../AnimatedLineChartScaleBand/AnimatedLineChart';
-import {lorenzobertoliniTheme} from './themes/lorenzobertolini';
-import {nerdyTheme} from './themes/nerdy';
+import {lorenzobertoliniTheme} from '../acetti-themes/lorenzobertolini';
+import {nerdyTheme} from '../acetti-themes/nerdy';
+import {DataColors} from './DataColors';
 
 export const zTheme = z.object({
 	global: z.object({
@@ -202,35 +203,8 @@ export const NerdyPriceChart3: React.FC<
 				timeSeries={timeSeries}
 				theme={theme}
 			/>
-			<AbsoluteFill>
-				<div>
-					<h1
-						style={{fontSize: 30, color: theme.xAxis.color, marginBottom: 16}}
-					>
-						DataColors
-					</h1>
-					<div style={{display: 'flex', gap: 20}}>
-						{theme.dataColors.map((dataColorObj, i) => {
-							const dataColor = dataColorObj.BASE;
-							return (
-								<div
-									style={{
-										backgroundColor: dataColor,
-										borderRadius: 3,
-										paddingTop: 6,
-										paddingBottom: 6,
-										paddingLeft: 10,
-										paddingRight: 10,
-									}}
-								>
-									<div>dataColor[{i}]</div>
-									<div>{dataColor}</div>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</AbsoluteFill>
+			<DataColors />
+			{/* <DataColors dataColors={theme.dataColors}/> */}
 		</AbsoluteFill>
 	);
 };
