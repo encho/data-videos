f8a44fe638bf45cccd54eb2fc6e1e52900e5120f
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-10-28 20:41:43 +0100

    useElementDimensions in own file

--- src/compositions/POCs/03-Page/SimplePage/SimplePageComposition.tsx
+++ src/compositions/POCs/03-Page/SimplePage/SimplePageComposition.tsx
@@ -1,16 +1,13 @@
-import React, {useRef, useEffect, useState, ReactNode} from 'react';
-import {continueRender, delayRender} from 'remotion';
+import React, {ReactNode} from 'react';
 import {z} from 'zod';
 
+import {useElementDimensions} from './useElementDimensions';
 import {ThemeType} from '../../../../acetti-themes/themeTypes';
 import {ThemePage, PageFooter, PageLogo} from './ThemePage';
 import {zThemeEnum} from '../../../../acetti-themes/getThemeFromEnum';
 import {SimpleBarChart} from '../../../../acetti-flics/SimpleBarChart/SimpleBarChart';
-// import {CapSizeTextNew} from '../../../../acetti-typography/CapSizeTextNew';
 import {EconomistTitleWithSubtitle} from '../../05-BarCharts/EconomistTitleWithSubtitle';
 import {useFontFamiliesLoader} from '../../../../acetti-typography/useFontFamiliesLoader';
-import {getBarChartBaseline} from '../../../../acetti-flics/SimpleBarChart/useBarChartLayout';
-// import {ThemeType} from '../../../../acetti-themes/themeTypes';
 import {TypographyStyle} from '../../02-TypographicLayouts/TextStyles/TextStylesComposition';
 import {BaselineGrid} from '../../02-TypographicLayouts/BaselineGrid/BaselineGrid';
 import {VerticalBaselineGrid} from '../../02-TypographicLayouts/BaselineGrid/VerticalBaselineGrid';
@@ -190,66 +187,3 @@ export const PerfectPage: React.FC<{
 		</ThemePage>
 	);
 };
-
-function useElementDimensions() {
-	// Create a ref to the div you want to measure
-	const divRef = useRef<HTMLDivElement>(null);
-
-	// State to store dimensions
-	const [dimensions, setDimensions] = useState<{
-		width: number;
-		height: number;
-	} | null>(null);
-
-	// Handle to delay rendering
-	const [handle] = useState(() => delayRender());
-
-	useEffect(() => {
-		const waitFirstTime = async () => {
-			// ***********************************************************
-			// TODO bring these waiting checks into the fonts loader!!!!!
-			// ***********************************************************
-			// await for when the fonts are loaded in the browser
-			await document.fonts.ready;
-
-			// Introduce an additional delay to ensure styles are applied
-			// await new Promise((resolve) => setTimeout(resolve, 2000));
-			await new Promise((resolve) => setTimeout(resolve, 1000));
-		};
-
-		const measure = async () => {
-			// const handle = delayRender();
-			// ***********************************************************
-			// TODO bring these waiting checks into the fonts loader!!!!!
-			// ***********************************************************
-			// await for when the fonts are loaded in the browser
-			// await document.fonts.ready;
-
-			// Introduce an additional delay to ensure styles are applied
-			// await new Promise((resolve) => setTimeout(resolve, 2000));
-			// await new Promise((resolve) => setTimeout(resolve, 200));
-			// ***********************************************************
-
-			if (divRef.current) {
-				const {offsetWidth, offsetHeight} = divRef.current;
-				setDimensions({width: offsetWidth, height: offsetHeight});
-				// Once measurement is done, continue rendering
-				continueRender(handle);
-			}
-		};
-
-		// Measure after the component has mounted and rendered
-		waitFirstTime().then(() => measure());
-		// measure();
-
-		// Optionally, add a resize listener if dimensions might change
-		// window.addEventListener('resize', measure);
-
-		// Cleanup on unmount
-		return () => {
-			// window.removeEventListener('resize', measure);
-		};
-	}, [handle]);
-
-	return {ref: divRef, dimensions};
-}
