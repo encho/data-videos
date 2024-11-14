20e40b94dc3daca79ea18eacf108c2ee71150d8d
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-11-14 17:07:20 +0100

    at 132 errors

--- src/compositions/POCs/05-BarCharts/ComposedSimpleBarChart/ComposedSImpleBarChartFlic.tsx
+++ src/compositions/POCs/05-BarCharts/ComposedSimpleBarChart/ComposedSImpleBarChartFlic.tsx
@@ -103,12 +103,11 @@ export const ComposedSimpleBarChartFlic: React.FC<{
 
 							<HtmlArea area={rightArea}>
 								<SimpleBarChart
+									hideLabels
 									theme={theme}
 									data={rightData}
 									width={rightArea.width}
 									height={rightArea.height}
-									hideLabels
-									// showLayout
 								/>
 							</HtmlArea>
 						</div>
@@ -116,10 +115,7 @@ export const ComposedSimpleBarChartFlic: React.FC<{
 				</div>
 
 				{/* TODO introduce evtl. also absolute positioned footer */}
-				<PageFooter
-					theme={theme}
-					// showArea={showAreas}
-				>
+				<PageFooter>
 					<div
 						style={{
 							display: 'flex',
@@ -138,64 +134,7 @@ export const ComposedSimpleBarChartFlic: React.FC<{
 					</div>
 				</PageFooter>
 			</div>
-			<PageLogo theme={theme} />
+			<PageLogo />
 		</Page>
 	);
 };
-
-function formatPercentage(value: number): string {
-	return (
-		(value * 100).toLocaleString(undefined, {
-			minimumFractionDigits: 1,
-			maximumFractionDigits: 1,
-		}) + '%'
-	);
-}
-// Example usage:
-// console.log(formatPercentage(0.12)); // Output: "12.0%"
-
-const wahlergebnis2024: {
-	parteiName: string;
-	prozent: number;
-	farbe: string;
-	id: string;
-}[] = [
-	{parteiName: 'SPD', prozent: 30.9 / 100, farbe: '#E3000F', id: 'SPD'}, // SPD Red
-	{parteiName: 'AfD', prozent: 29.2 / 100, farbe: '#009EE0', id: 'AFD'}, // AfD Blue
-	{parteiName: 'BSW', prozent: 13.5 / 100, farbe: '#FFA500', id: 'BSW'}, // BSW Orange (aligned with Sahra Wagenknecht's movement)
-	// {parteiName: 'CDU', prozent: 12.1 / 100, farbe: '#000000'}, // CDU Black
-	{parteiName: 'CDU', prozent: 12.1 / 100, farbe: '#fff', id: 'CDU'}, // CDU Black
-	{parteiName: 'Grüne', prozent: 4.1 / 100, farbe: '#64A12D', id: 'GRU'}, // Grüne Green
-	{parteiName: 'Die Linke', prozent: 3.0 / 100, farbe: '#BE3075', id: 'LIN'}, // Die Linke Magenta
-	{
-		parteiName: 'BVB',
-		prozent: 2.6 / 100,
-		farbe: '#FFD700',
-		id: 'BVB',
-	}, // BVB Yellow
-	{parteiName: 'FDP', prozent: 0.8 / 100, farbe: '#FFED00', id: 'FDP'}, // FDP Yellow
-	{parteiName: 'Sonstige', prozent: 4.6 / 100, farbe: '#808080', id: 'SON'}, // Others Gray
-];
-
-const wahlergebnis2024_percChange: {
-	parteiName: string;
-	change: number;
-	farbe: string;
-	id: string;
-}[] = [
-	{parteiName: 'SPD', change: -0.1, farbe: '#E3000F', id: 'SPD'}, // SPD Red
-	{parteiName: 'AfD', change: -0.3, farbe: '#009EE0', id: 'AFD'}, // AfD Blue
-	{parteiName: 'BSW', change: 0.5, farbe: '#FFA500', id: 'BSW'}, // BSW Orange (aligned with Sahra Wagenknecht's movement)
-	// {parteiName: 'CDU', change: 12.1 / 100, farbe: '#000000'}, // CDU Black
-	{parteiName: 'CDU', change: 0.2, farbe: '#fff', id: 'CDU'}, // CDU Black
-	{parteiName: 'Grüne', change: -0.05, farbe: '#64A12D', id: 'GRU'}, // Grüne Green
-	{parteiName: 'Die Linke', change: 0.1, farbe: '#BE3075', id: 'LIN'}, // Die Linke Magenta
-	{
-		parteiName: 'BVB',
-		change: 0.02,
-		farbe: '#FFD700',
-		id: 'BVB',
-	}, // BVB Yellow
-	{parteiName: 'FDP', change: -0.8, farbe: '#FFED00', id: 'FDP'}, // FDP Yellow
-	{parteiName: 'Sonstige', change: 0.9, farbe: '#808080', id: 'SON'}, // Others Gray
-];
