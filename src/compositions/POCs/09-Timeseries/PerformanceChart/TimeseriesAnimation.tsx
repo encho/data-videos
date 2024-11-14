f9a859e5fdbe4f2dee9a7b3d555b0a41c7188530
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-11-14 21:20:56 +0100

    down to 5 errors

--- src/compositions/POCs/09-Timeseries/PerformanceChart/TimeseriesAnimation.tsx
+++ src/compositions/POCs/09-Timeseries/PerformanceChart/TimeseriesAnimation.tsx
@@ -27,11 +27,37 @@ export const TimeseriesAnimation: React.FC<TAnimatedLineChart2Props> = ({
 	timeSeries,
 	theme,
 }) => {
-	const {durationInFrames} = useVideoConfig();
-
 	const CHART_PAGE_MARGIN = 40;
 	const CHART_PAGE_WIDTH = width;
 	const CHART_PAGE_HEIGHT = height;
+
+	return (
+		<div style={{position: 'relative'}}>
+			<div>
+				<PageContext
+					width={CHART_PAGE_WIDTH}
+					height={CHART_PAGE_HEIGHT}
+					margin={CHART_PAGE_MARGIN}
+					nrBaselines={30}
+					theme={theme}
+				>
+					<OpacifyInAndOut>
+						<Page boxShadow borderRadius={5}>
+							<TimeseriesAnimationInside timeSeries={timeSeries} />
+						</Page>
+					</OpacifyInAndOut>
+				</PageContext>
+			</div>
+		</div>
+	);
+};
+
+export const TimeseriesAnimationInside: React.FC<{
+	timeSeries: {value: number; date: Date}[];
+}> = ({timeSeries}) => {
+	const {durationInFrames} = useVideoConfig();
+
+	const {contentWidth, contentHeight} = usePage();
 
 	// TODO use keyframes perhaps
 	const td_buildup = Math.floor(durationInFrames * 0.5);
@@ -71,92 +97,70 @@ export const TimeseriesAnimation: React.FC<TAnimatedLineChart2Props> = ({
 		],
 	});
 
+	const xAxisHeight = useXAxisAreaHeight();
+
+	const yScaleAnimationUpper = useYScaleAnimation({
+		periodScaleAnimation,
+		timeSeriesArray: [timeSeries],
+		tickFormatter: (tick) => `${tick} $`,
+		yScalesInitialHeight: 200,
+		domainType: 'VISIBLE',
+		paddingPerc: 0.1,
+	});
+
+	const yAxisWidth = yScaleAnimationUpper.maxLabelComponentWidth;
+
+	const chartLayout = useChartLayout({
+		width: contentWidth,
+		height: contentHeight,
+		yAxisWidth,
+		xAxisHeight,
+	});
+
 	return (
-		<div style={{position: 'relative'}}>
-			<div>
-				<PageContext
-					width={CHART_PAGE_WIDTH}
-					height={CHART_PAGE_HEIGHT}
-					margin={CHART_PAGE_MARGIN}
-					nrBaselines={30}
-					theme={theme}
+		<div
+			style={{
+				position: 'relative',
+			}}
+		>
+			<PerformanceChart
+				periodScaleAnimation={periodScaleAnimation}
+				yScaleAnimation={yScaleAnimationUpper}
+				timeSeries={timeSeries}
+				layoutAreas={{
+					xAxis: chartLayout.areas.xAxis,
+					plot: chartLayout.areas.plot,
+					yAxis: chartLayout.areas.yAxis,
+				}}
+			/>
+			<Sequence from={percentageChange_enter_start_keyframe}>
+				<Position
+					position={{
+						left: chartLayout.areas.plot.x1,
+						top: chartLayout.areas.plot.y1,
+					}}
 				>
-					<OpacifyInAndOut>
-						<Page boxShadow borderRadius={5}>
-							{() => {
-								const {contentWidth, contentHeight} = usePage();
+					{/* TODO port to Animated_Line */}
+					<Animated_PercentageChangeArea
+						// periodsScale={periodScaleAnimation.periodsScale}
+						yScale={yScaleAnimationUpper.yScale}
+						//
+						area={chartLayout.areas.plot}
+						firstValue={timeSeries[0].value}
+						lastValue={timeSeries[timeSeries.length - 1].value}
+					/>
+				</Position>
+			</Sequence>
 
-								const xAxisHeight = useXAxisAreaHeight();
-
-								const yScaleAnimationUpper = useYScaleAnimation({
-									periodScaleAnimation,
-									timeSeriesArray: [timeSeries],
-									tickFormatter: (tick) => `${tick} $`,
-									yScalesInitialHeight: 200,
-									domainType: 'VISIBLE',
-									paddingPerc: 0.1,
-								});
-
-								const yAxisWidth = yScaleAnimationUpper.maxLabelComponentWidth;
-
-								const chartLayout = useChartLayout({
-									width: contentWidth,
-									height: contentHeight,
-									yAxisWidth,
-									xAxisHeight,
-								});
-
-								return (
-									<div
-										style={{
-											position: 'relative',
-										}}
-									>
-										<PerformanceChart
-											periodScaleAnimation={periodScaleAnimation}
-											yScaleAnimation={yScaleAnimationUpper}
-											timeSeries={timeSeries}
-											layoutAreas={{
-												xAxis: chartLayout.areas.xAxis,
-												plot: chartLayout.areas.plot,
-												yAxis: chartLayout.areas.yAxis,
-											}}
-										/>
-										<Sequence from={percentageChange_enter_start_keyframe}>
-											<Position
-												position={{
-													left: chartLayout.areas.plot.x1,
-													top: chartLayout.areas.plot.y1,
-												}}
-											>
-												{/* TODO port to Animated_Line */}
-												<Animated_PercentageChangeArea
-													// periodsScale={periodScaleAnimation.periodsScale}
-													yScale={yScaleAnimationUpper.yScale}
-													//
-													area={chartLayout.areas.plot}
-													firstValue={timeSeries[0].value}
-													lastValue={timeSeries[timeSeries.length - 1].value}
-												/>
-											</Position>
-										</Sequence>
-
-										<div style={{position: 'absolute', top: 0, left: 0}}>
-											<DisplayGridLayout
-												hide
-												stroke="rgba(255,0,255,0.8)"
-												fill="transparent"
-												areas={chartLayout.areas}
-												width={contentWidth}
-												height={contentHeight}
-											/>
-										</div>
-									</div>
-								);
-							}}
-						</Page>
-					</OpacifyInAndOut>
-				</PageContext>
+			<div style={{position: 'absolute', top: 0, left: 0}}>
+				<DisplayGridLayout
+					hide
+					stroke="rgba(255,0,255,0.8)"
+					fill="transparent"
+					areas={chartLayout.areas}
+					width={contentWidth}
+					height={contentHeight}
+				/>
 			</div>
 		</div>
 	);
