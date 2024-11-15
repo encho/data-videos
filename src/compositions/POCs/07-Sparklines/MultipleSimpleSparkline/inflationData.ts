efbbbd6581f2cfecd69b90467d4a513133f33333
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-11-15 13:59:41 +0100

    fix types

--- src/compositions/POCs/07-Sparklines/MultipleSimpleSparkline/inflationData.ts
+++ src/compositions/POCs/07-Sparklines/MultipleSimpleSparkline/inflationData.ts
@@ -1,10 +1,7 @@
 import {TimeSeries} from '../../../../acetti-ts-utils/timeSeries/timeSeries';
 
 // Slide Title and Data Source
-const slideTitle: string =
-	// 'Comparative Inflation Rates Across Countries (2022-2023)';
-	// 'Comparative Inflation';
-	'United States Leads in Reducing Inflation Rates';
+const slideTitle: string = 'United States Leads in Reducing Inflation Rates';
 
 const slideSubtitle: string =
 	'Comparative Inflation Rates Across Countries (2022-2023)';
@@ -12,7 +9,7 @@ const slideSubtitle: string =
 const dataSource: string =
 	'Data sourced from official statistics bureaus of each country.';
 
-const usInflationTimeseries: Timeseries = [
+const usInflationTimeseries: TimeSeries = [
 	{date: new Date('2022-01-31'), value: 7.5},
 	{date: new Date('2022-02-28'), value: 7.9},
 	{date: new Date('2022-03-31'), value: 8.5},
@@ -35,7 +32,7 @@ const usInflationTimeseries: Timeseries = [
 	{date: new Date('2023-08-31'), value: 3.7},
 ];
 
-const ukInflationTimeseries: Timeseries = [
+const ukInflationTimeseries: TimeSeries = [
 	{date: new Date('2022-01-31'), value: 5.5},
 	{date: new Date('2022-02-28'), value: 6.2},
 	{date: new Date('2022-03-31'), value: 7.0},
@@ -58,7 +55,7 @@ const ukInflationTimeseries: Timeseries = [
 	{date: new Date('2023-08-31'), value: 6.7},
 ];
 
-const germanyInflationTimeseries: Timeseries = [
+const germanyInflationTimeseries: TimeSeries = [
 	{date: new Date('2022-01-31'), value: 4.9},
 	{date: new Date('2022-02-28'), value: 5.1},
 	{date: new Date('2022-03-31'), value: 7.3},
@@ -81,7 +78,7 @@ const germanyInflationTimeseries: Timeseries = [
 	{date: new Date('2023-08-31'), value: 6.1},
 ];
 
-const japanInflationTimeseries: Timeseries = [
+const japanInflationTimeseries: TimeSeries = [
 	{date: new Date('2022-01-31'), value: 0.5},
 	{date: new Date('2022-02-28'), value: 0.9},
 	{date: new Date('2022-03-31'), value: 1.2},
@@ -105,7 +102,7 @@ const japanInflationTimeseries: Timeseries = [
 ];
 
 // Sparklines Array
-const sparklines: {title: string; timeseries: Timeseries}[] = [
+const sparklines: {title: string; timeseries: TimeSeries}[] = [
 	{
 		// title: 'United States Inflation Rate',
 		title: 'United States',
@@ -141,7 +138,7 @@ type TSparklineProps = {
 	title: string;
 	subtitle: string;
 	dataSource: string;
-	sparklines: {title: string; timeseries: Timeseries}[];
+	sparklines: {title: string; timeseries: TimeSeries}[];
 	formatString: string;
 };
 
