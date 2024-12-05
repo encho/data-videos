b31823ec84172318931ac313a6c53ec91056cd39
Lorenzo Bertolini <ciao@lorenzobertolini.com>
2024-12-05 11:34:16 +0100

    update nerdy tickers

--- src/acetti-http/nerdy-finance/types/nerdyFinance_types_availableStrategies.ts
+++ src/acetti-http/nerdy-finance/types/nerdyFinance_types_availableStrategies.ts
@@ -1,6 +1,6 @@
 
 import { z } from "zod";
 
-export const zNerdyFinance_availableStrategies = z.enum(["BITCOIN_ONLY","GOLD_ONLY","BITCOIN_GOLD_VOLA_BALANCED_360","GOLD_CRYPTO_60_40"]);
+export const zNerdyFinance_availableStrategies = z.enum(["BITCOIN_ONLY","GOLD_ONLY","BITCOIN_GOLD_VOLA_BALANCED_360","GOLD_CRYPTO_80_20","GOLD_CRYPTO_60_40"]);
 
 export type TNerdyFinance_availableStrategies = z.infer<typeof zNerdyFinance_availableStrategies>;
